9a88cbe3d91234d46525cf97297b69e1a78bbe58
DarthHamza <hamzamakia@gmail.com>
2021-01-14 16:47:32 +0300

    changed to deck of crads

--- stacks.js
+++ stacks.js
@@ -44,17 +44,14 @@ class Stack {
   };
 }
 
-const colors = ["red", "blue", "green", "yellow"];
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const colors = ["hearts", "spades", "clubs", "diamonds"];
+const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
 
-const deck = new Stack();
+const deck = new Stack(52);
 const random = (array) => {
   const randomIndex = Math.floor(Math.random() * array.length);
   return array[randomIndex];
 };
-
-console.log(random(colors));
-console.log(random(numbers));
 
 console.log(random(colors));
 console.log(random(numbers));
