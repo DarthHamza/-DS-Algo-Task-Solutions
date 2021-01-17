d2d21415705f90d29c852dc8b17997d5f4bcc498
DarthHamza <hamzamakia@gmail.com>
2021-01-17 11:57:46 +0300

    stacks solution

--- stacks.js
+++ stacks.js
@@ -5,7 +5,7 @@ class Node {
     this.next = next;
   }
 
-  getData = () => `${this.color} - ${this.number}`;
+  getData = () => `${this.number} ${this.color}`;
 }
 
 class Stack {
@@ -42,9 +42,17 @@ class Stack {
       return popped.getData();
     }
   };
+
+  traverse = () => {
+    let current = this.top;
+    while (current) {
+      console.log(current.getData());
+      current = current.next;
+    }
+  };
 }
 
-const colors = ["hearts", "spades", "clubs", "diamonds"];
+const colors = ["♥️️", "♠️️", "♣️️", "♦️️"];
 const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
 
 const deck = new Stack(52);
@@ -53,5 +61,22 @@ const random = (array) => {
   return array[randomIndex];
 };
 
-console.log(random(colors));
-console.log(random(numbers));
+while (!deck.isFull()) {
+  let color = random(colors);
+  let number = random(numbers);
+  deck.push(color, number);
+}
+
+let playerCards = 5;
+let player1Cards = [];
+let player2Cards = [];
+for (let i = 0; i < 5; i++) {
+  player1Cards.push(deck.pop());
+  player2Cards.push(deck.pop());
+}
+
+console.table(player1Cards);
+console.table(player2Cards);
+console.log(deck.peek());
+console.log("---------------");
+deck.traverse();
